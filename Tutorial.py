557acf41dfd2ebee72fcce9a25394053a03b532e
Monty Python <mvpcomputerrepair@gmail.com>
2016-06-13 23:10:12 -0400

    added while loops

--- Tutorial.py
+++ Tutorial.py
@@ -232,5 +232,30 @@ num_list =[[1,2,3],[10,20,30],[100,200,300]];
 for x in range(0,3):
     for y in range(0,3):
         print(num_list[x][y])
+        
+# WHILE LOOPS -------------
+# While loops are used when you don't know ahead of time how many
+# times you'll have to loop
+random_num = random.randrange(0,100)
+ 
+while (random_num != 15):
+    print(random_num)
+    random_num = random.randrange(0,100)
+ 
+# An iterator for a while loop is defined before the loop
+i = 0;
+while (i <= 20):
+    if(i%2 == 0):
+        print(i)
+    elif(i == 9):
+        # Forces the loop to end all together
+        break
+    else:
+        # Shorthand for i = i + 1
+        i += 1
+        # Skips to the next iteration of the loop
+        continue
+ 
+    i += 1
  
     
